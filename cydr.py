041806e3e22d7d1d34eea77832caf25a63b3a237
JT <68863931+jtobinart@users.noreply.github.com>
2024-07-14 01:01:31 -0400

    Add files via upload
    Added the ability to specify the width and height of the display when initializing cyd class.
    
    Example:
         cyd = CYD(rgb_pmw=False, speaker_gain=512, display_width=240, display_height=320)

--- cydr.py
+++ cydr.py
@@ -4,7 +4,7 @@
 # Author(s): James Tobin
 # License: MIT
 # https://github.com/jtobinart/MicroPython_CYD_ESP32-2432S028R
-
+print("loading...")
 ######################################################
 #   MIT License
 ######################################################
@@ -41,9 +41,9 @@ v1.1
 
 v1.0
     This is a higher-level library to control DIYmall's ESP32-2432S028R, also known as the Cheap Yellow Display (CYD).
-    
+
 TO DO:
-    - Implement continuous touch 
+    - Implement continuous touch
     - Implement DAC pin 26 for the speaker instead of using PWM
     - SD card creates a critical error when using keyboard interrupt
     - Implement easy Bluetooth functions
@@ -85,9 +85,9 @@ Pins
     33   Digital   Touch XPT2046              - CS [Software SPI]
     34   Analog    LDR Light Sensor           - !!!Input ONLY!!!
     35   Digital   P3 Connector               - !!!Input ONLY w/ NO pull-ups!!!
-    36   Digital   Touch XPT2046              - IRQ !!!Input ONLY!!! 
+    36   Digital   Touch XPT2046              - IRQ !!!Input ONLY!!!
     39   Digital   Touch XPT2046              - MISO !!!Input ONLY!!! [Software SPI]
-   
+
 """
 
 ######################################################
@@ -115,7 +115,7 @@ class CYD(object):
     #   Function List
     ######################################################
     '''
-        cyd = CYD(rgb_pmw=False, speaker_gain=512)      # Initialize CYD class
+        cyd = CYD(rgb_pmw=False, speaker_gain=512, display_width=240, display_height=320)      # Initialize CYD class
         cyd.display.ili9341_function_name()             # Use to access ili9341 functions.
         cyd._touch_handler(x, y)                        # Called when a touch occurs. (INTERNAL USE ONLY)
         cyd.touches()                                   # GETS the last touch coordinates.
@@ -126,11 +126,11 @@ class CYD(object):
         cyd.button_boot()                               # GETS the current boot button value.
         cyd.backlight(value)                            # SETS backlight brightness.
         cyd.play_tone(freq, duration, gain=0)           # Plays a tone for a given duration.
-        cyd.mount_sd()                                  # Mounts SD card         
+        cyd.mount_sd()                                  # Mounts SD card
         cyd.unmount_sd()                                # Unmounts SD card.
         cyd.shutdown()                                  # Safely shutdown CYD device.
     '''
-    def __init__(self, rgb_pmw=False, speaker_gain=512):
+    def __init__(self, rgb_pmw=False, speaker_gain=512, display_width=240, display_height=320):
         '''
         Initialize CDYc
 
@@ -138,32 +138,32 @@ class CYD(object):
             rgb_pmw (Default = False): Sets RGB LED to static mode. (on/off), if false
                                        Sets RGB LED to dynamic mode. (16.5+ million color combination), if true
                                        Warning: RGB LED never completely turns off in dynamic mode.
-            
-            speaker_gain (Default = 512): Sets speaker's volume. The full gain range is 0 - 1023.
 
-            sd_enabled (Default = False): Initializes SD Card reader, user still needs to run mount_sd() to access SD card.
+            speaker_gain (Default = 512): Sets speaker's volume. The full gain range is 0 - 1023.
+            display_width (Default = 240): Reset if needed.
+            display_height (Default = 320): Reset if needed.
         '''
         # Display
         hspi = SPI(1, baudrate=40000000, sck=Pin(14), mosi=Pin(13))
-        self.display = Display(hspi, dc=Pin(2), cs=Pin(15), rst=Pin(0))
+        self.display = Display(hspi, dc=Pin(2), cs=Pin(15), rst=Pin(0), width=display_width, height=display_height)
         self._x = 0
         self._y = 0
-        
+
         # Backlight
         self.tft_bl = Pin(21, Pin.OUT)
-        self.tft_bl.value(1) #Turn on backlight 
-        
+        self.tft_bl.value(1) #Turn on backlight
+
         # Touch
         self.last_tap = (-1,-1)
         sspi = SoftSPI(baudrate=500000, sck=Pin(25), mosi=Pin(32), miso=Pin(39))
         self._touch = Touch(sspi, cs=Pin(33), int_pin=Pin(36), int_handler=self._touch_handler)
-        
+
         # Boot Button
         self._button_boot = Pin(0, Pin.IN)
-        
+
         # LDR: Light Sensor (Measures Darkness)
         self._ldr = ADC(34)
-        
+
         # RGB LED
         self._rgb_pmw = rgb_pmw
         if self._rgb_pmw == False:
@@ -175,19 +175,20 @@ class CYD(object):
             self.RGBg = PWM(Pin(16), freq=200, duty=1023)    # Green
             self.RGBb = PWM(Pin(17), freq=200, duty=1023)    # Blue
             print("RGB PMW Ready")
-        
+
         # Speaker
         self._speaker_pin = Pin(26, Pin.OUT)
         self.speaker_gain = int(min(max(speaker_gain, 0),1023))     # Min 0, Max 1023
         self.speaker_pwm = PWM(self._speaker_pin, freq=440, duty=0)
-            
+
         # SD Card
+        # The user needs to run mount_sd() to access SD card.
         self._sd_ready = False
         self._sd_mounted = False
-    
+
     ######################################################
     #   Touchscreen Press Event
-    ###################################################### 
+    ######################################################
     def _touch_handler(self, x, y):
         '''
         Interrupt Handler
@@ -200,27 +201,27 @@ class CYD(object):
         self._y = y
 
         #print("Touch:", x, y)
-    
+
     def touches(self):
         '''
         Returns last stored touch data.
-        
+
         Return:
             x: x coordinate of finger 1
             y: y coordinate of finger 1
         '''
         x = self._x
         y = self._y
-        
+
         self._x = 0
         self._y = 0
-        
+
         return x, y
-    
+
     def double_tap(self, x, y, error_margin = 5):
         '''
         Returns whether or not a double tap was detected.
-        
+
         Return:
             True: Double-tap detected.
             False: Single tap detected.
@@ -232,14 +233,14 @@ class CYD(object):
                 return True
         self.last_tap = (x,y)
         return False
-        
+
     ######################################################
     #   RGB LED
-    ###################################################### 
+    ######################################################
     def rgb(self, color):
         '''
         Set RGB LED color.
-        
+
         Args:
             color: Array containing three int values (r,g,b).
                     if rgb_pmw == False, then static mode is activated.
@@ -260,7 +261,7 @@ class CYD(object):
             self.RGBr.duty(int(min(max(self._remap(r,0,255,1023,0), 0),1023)))
             self.RGBg.duty(int(min(max(self._remap(g,0,255,1023,0), 0),1023)))
             self.RGBb.duty(int(min(max(self._remap(b,0,255,1023,0), 0),1023)))
-    
+
     def _remap(self, value, in_min, in_max, out_min, out_max):
         '''
         Internal function for remapping values from one scale to a second.
@@ -269,46 +270,46 @@ class CYD(object):
         out_span = out_max - out_min
         scale = out_span / in_span
         return out_min + (value - in_min) * scale
-    
+
     ######################################################
     #   Light Sensor
-    ###################################################### 
+    ######################################################
     def light(self):
         '''
         Light Sensor (Measures darkness)
-        
+
         Return: a value from 0.0 to 1.0
         '''
         return self._ldr.read_u16()/65535
-    
+
     ######################################################
     #   Button
-    ###################################################### 
+    ######################################################
     def button_boot(self):
         '''
         Gets the Boot button's current state
         '''
         return self._button_boot.value
-    
+
     ######################################################
     #   Backlight
-    ###################################################### 
+    ######################################################
     def backlight(self, val):
         '''
         Sets TFT Backlight Off/On
-        
+
         Arg:
             val: 0 or 1 (0 = off/ 1 = on)
         '''
         self.tft_bl.value(min(max(val, 0),1))
-    
+
     ######################################################
     #   Speaker
-    ###################################################### 
+    ######################################################
     def play_tone(self, freq, duration, gain=0):
         '''
         Plays a tone (Optional speaker must be attached!)
-        
+
         Args:
             freq: Frequency of the tone.
             duration: How long does the tone play for.
@@ -320,10 +321,10 @@ class CYD(object):
         self.speaker_pwm.duty(gain)             # Turn on speaker by resetting speaker gain
         time.sleep_ms(duration)
         self.speaker_pwm.duty(0)                # Turn off speaker by resetting gain to zero
-    
+
     ######################################################
     #   SD Card
-    ###################################################### 
+    ######################################################
     def mount_sd(self):
         '''
         Mounts SD Card
@@ -339,7 +340,7 @@ class CYD(object):
 
         except:
             print("Failed to mount SD card")
-    
+
     def unmount_sd(self):
         '''
         Unmounts SD Card
@@ -351,10 +352,10 @@ class CYD(object):
                 print("SD card unmounted. Safe to remove SD card!")
         except:
             print("Failed to unmount SD card")
-    
+
     ######################################################
     #   Shutdown
-    ###################################################### 
+    ######################################################
     def shutdown(self):
         '''
         Resets CYD and properly shuts down.
